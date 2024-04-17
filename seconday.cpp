09ce4a21fdbdea1463851ce9c531dfbb19a777aa
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-04-17 10:56:04 +0530

    Add files via upload

---
+++ seconday.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a,b;
+cout<<"Enter the first Number: ";
+cin>>a;
+cout<<"\nEnter the second Number: ";
+cin>>b;
+cout<<endl;
+cout<<"Addition of two numbers is: "<<a+b;
+};
