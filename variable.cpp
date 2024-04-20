489492d8425be5a8772e7f8a862642ccfcc3f17e
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-04-21 01:49:40 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Variable Declaration
+    int a;
+    // Variable Definition
+    a=10;
+    cout<<a<<endl<<"a\n";
+    bool b=true;
+    cout<<int(b);
+};
